71d91c220abec4b5ecb96062a9a58e895e8d28bc
Ehab Yasser <eng.ehabyasser@gmail.com>
2023-11-13 17:45:41 +0200

    adding utils functions

--- utils.py
+++ utils.py
@@ -1,59 +1,54 @@
-import random
+def IS_GAME_OVER(bitboard):
+    bitboard >>= 6
+    for col in range(7):
+        if ((bitboard >> (col * 9)) & 0b111) != 0:
+            return False
+    return True
 
-EMPTY = 0
-HUMAN = 1
-COMPUTER = 2
-ROWS = 6
-COLS = 7
+def CAN_MAKE_MOVE(bitboard, column):
+    print((bitboard >> ((6 - column) * 9) + 6) & 0b111)
+    return ((bitboard >> ((6 - column) * 9) + 6) & 0b111) != 0
 
+def MAKE_MOVE(bitboard, column, player):
+    col_bits = (bitboard >> ((6 - column) * 9)) & 0b111111111
+    index = (col_bits >> 6) & 0b111
 
-def initialize_board():
-    return [[EMPTY] * COLS for _ in range(ROWS)]
+    if index == 0:
+        return bitboard
 
-def is_valid_move(board, col):
-    return 0 <= col <= 6 and board[0][col] == EMPTY
+    col_bits |= player << (6 - index)
+    index -= 1
+    col_bits &= 0b000111111
+    col_bits |= index << 6
+    bitboard &= ~(0b111111111 << ((6 - column) * 9))
+    bitboard |= col_bits << ((6 - column) * 9)
 
+    return bitboard
 
-def is_board_full(board):
-    return all(cell != EMPTY for row in board for cell in row)
-
-
-def count_connected_fours(board, player):
-    count = 0
-
-    # Check horizontally
-    for row in range(ROWS):
-        for col in range(COLS - 3):
-            if all(board[row][col + i] == player for i in range(4)):
-                count += 1
-
-    # Check vertically
-    for row in range(ROWS - 3):
-        for col in range(COLS):
-            if all(board[row + i][col] == player for i in range(4)):
-                count += 1
-
-    # Check diagonally (down-right)
-    for row in range(ROWS - 3):
-        for col in range(COLS - 3):
-            if all(board[row + i][col + i] == player for i in range(4)):
-                count += 1
-
-    # Check diagonally (up-right)
-    for row in range(3, ROWS):
-        for col in range(COLS - 3):
-            if all(board[row - i][col + i] == player for i in range(4)):
-                count += 1
-
-    return count
+def GET_POSSIBLE_MOVES(bitboard):
+    moves = []
+    bitboard >>= 6
+    for col in range(7):
+        if ((bitboard >> (col * 9)) & 0b111) != 0:
+            moves.insert(0, 6 - col)
+    return moves
 
 
-def finalize_game(board):
-    human_count = count_connected_fours(board, HUMAN)
-    computer_count = count_connected_fours(board, COMPUTER)
-    if human_count > computer_count:
-        return "You won", "blue"
-    elif human_count < computer_count:
-        return "Computer won", "red"
-    else:
-        return "It's a draw!", "green"
+# Example usage with the corrected understanding of column full/empty
+initial_bitboard = int("100000001110000000110000000110000000101000001110000000000101010", 2)
+new_bitboard = MAKE_MOVE(initial_bitboard, 6, 0)
+print(GET_POSSIBLE_MOVES(initial_bitboard))
+new_bitboard_bin = bin(new_bitboard)
+
+print(new_bitboard_bin)
+
+# 101000001110000000110000000110000000110000000110000000110000000
+# 100000001110000000110000000110000000110000000110000000110000000
+# 100000001110000000110000000110000000101000001110000000110000000
+# 100000001110000000110000000110000000101000001110000000101000000
+# 100000001110000000110000000110000000101000001110000000100000010
+# 100000001110000000110000000110000000101000001110000000011000010
+# 100000001110000000110000000110000000101000001110000000010001010
+# 100000001110000000110000000110000000101000001110000000001001010
+# 100000001110000000110000000110000000101000001110000000000101010
+# 100000001110000000110000000110000000101000001110000000000101010
