1cfb93954c80c889131ab6f9b53c84050bb46d75
Ehab Yasser <eng.ehabyasser@gmail.com>
2023-11-14 15:59:57 +0200

    gui fixes (tree is not finished yet)
    Co-Authored-By: Rowaina <96254289+rowaina2025@users.noreply.github.com>

--- algorithms/minimax_ab_pruning.py
+++ algorithms/minimax_ab_pruning.py
@@ -1,10 +1,7 @@
-import os
-import sys
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
 from utils import *
 from tree_node import TreeNode
 from heuristics import heuristic_evaluation
+
 
 class MinimaxAlphaBeta:
     def __init__(self, bitboard, max_depth, maximizing_player):
@@ -15,43 +12,50 @@ class MinimaxAlphaBeta:
         self.alpha = float("-inf")
         self.beta = float("inf")
 
-    def generate_minimax_tree(self, depth, bitboard, alpha, beta, maximizing_player):
-        if depth == 0 or IS_GAME_OVER(bitboard): # BASE CASE
-            return TreeNode(bitboard, depth, maximizing_player, heuristic_evaluation(bitboard))
-        
-        node = TreeNode(bitboard, depth, maximizing_player)
+    def generate_minimax_tree(self, depth, bitboard, alpha, beta, maximizing_player, parent):
+        if depth == 0 or IS_GAME_OVER(bitboard):  # BASE CASE
+            return TreeNode(bitboard, depth, heuristic_evaluation(bitboard), maximizing_player, parent)
+
+        node = TreeNode(bitboard, depth, heuristic_evaluation(bitboard), maximizing_player, parent)
 
         if maximizing_player:
             max_score = float("-inf")
-            for col in GET_POSSIBLE_MOVES(bitboard):
-                new_bitboard = MAKE_MOVE(bitboard, col, self.maximizing_player)
-                child_node = self.generate_minimax_tree(depth-1, new_bitboard, alpha, beta, False)
-                node.children.append(child_node)
-                max_score = max(max_score, child_node.score)
-                self.alpha = max(alpha, child_node.score)
-                self.path.add(new_bitboard)
-                if (self.beta <= self.alpha):
-                    break
+            for column in range(7):
+                if CAN_MAKE_MOVE(bitboard, column):
+                    new_bitboard = MAKE_MOVE(bitboard, column, self.maximizing_player)
+                    child_node = self.generate_minimax_tree(depth - 1, new_bitboard, alpha, beta, False, bitboard)
+                    node.children.append(child_node)
+                    max_score = max(max_score, child_node.score)
+                    self.alpha = max(alpha, child_node.score)
+                    self.path.add(new_bitboard)
+                    if self.beta <= self.alpha:
+                        break
             node.score = max_score
             return node
         else:
             min_score = float("inf")
-            for col in GET_POSSIBLE_MOVES(bitboard):
-                new_bitboard = MAKE_MOVE(bitboard, col, not self.maximizing_player)
-                child_node = self.generate_minimax_tree(depth-1, new_bitboard, alpha, beta, True)
-                node.children.append(child_node)
-                min_score = min(min_score, child_node.score)
-                self.beta = min(beta, child_node.score)
-                self.path.add(new_bitboard)
-                if (self.beta <= self.alpha):
-                    break
+            for column in range(7):
+                if CAN_MAKE_MOVE(bitboard, column):
+                    new_bitboard = MAKE_MOVE(bitboard, column, not self.maximizing_player)
+                    child_node = self.generate_minimax_tree(depth - 1, new_bitboard, alpha, beta, True, bitboard)
+                    node.children.append(child_node)
+                    min_score = min(min_score, child_node.score)
+                    self.beta = min(beta, child_node.score)
+                    self.path.add(new_bitboard)
+                    if self.beta <= self.alpha:
+                        break
             node.score = min_score
             return node
-        
+
     def solve(self):
-        root = self.generate_minimax_tree(self.max_depth, self.current_bitboard, self.alpha, self.beta, self.maximizing_player)
-        return root, self.path
-    
+        root = self.generate_minimax_tree(self.max_depth, self.current_bitboard, self.alpha, self.beta, self.maximizing_player, None)
+        max_child = root.children[0]
+        for child in root.children:
+            if child.score >= max_child.score:
+                max_child = child
+        changed_column = GET_CHANGED_COLUMN(root.bitboard, max_child.bitboard)
+        return changed_column, root, self.path
+
 if __name__ == '__main__':
     bitboard = 0b0110000000110000000110000000110000000110000000110000000110000000  # Empty Board
 
